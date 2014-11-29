7ce69bd53f8537a771f518b4fe7922eba175b255
Arnaud Debray <debray.arnaud@gmail.com>
2014-11-29 15:10:49 +0100

    auto-home push

---
+++ editor.lua
@@ -0,0 +1,18 @@
+local editor = {}
+
+function editor:init()
+	self.State = 'editor'
+
+	return self
+end
+
+function editor:update(dt)
+end
+
+function editor:draw()
+end
+
+function editor:keypressed()
+end
+
+return editor

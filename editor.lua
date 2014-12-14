568bc7c8d6c63b679d479674c3ad493a47e955a9
Arnaud Debray <debray.arnaud@gmail.com>
2014-12-14 21:06:46 +0100

    shapes + meshes hydratation

--- editor.lua
+++ editor.lua
@@ -1,5 +1,98 @@
 local editor = {}
 
+local Meshlist = {}
+
+function Meshlist.new(in_table, image, polygon)
+	local vertices = {
+		{
+			polygon.vertices[1].x, polygon.vertices[1].y,
+			0, 0,
+			255, 255, 255,
+		},
+		{
+			polygon.vertices[2].x, polygon.vertices[2].y,
+			1, 0,
+			255, 255, 255,
+		},
+		{
+			polygon.vertices[3].x, polygon.vertices[3].y,
+			1, 1,
+			255, 255, 255,
+		},
+		{
+			polygon.vertices[4].x, polygon.vertices[4].y,
+			0, 1,
+			255, 255, 255,
+		}
+	}
+	table.insert(in_table, {
+		mesh = love.graphics.newMesh( vertices, image, 'fan'),
+		move = function (self, x, y)
+			local vertices = self.mesh:getVertices()
+			vertices[1][1] = vertices[1][1] + x
+			vertices[1][2] = vertices[1][2] + y
+			vertices[2][1] = vertices[2][1] + x
+			vertices[2][2] = vertices[2][2] + y
+			vertices[3][1] = vertices[3][1] + x
+			vertices[3][2] = vertices[3][2] + y
+			vertices[4][1] = vertices[4][1] + x
+			vertices[4][2] = vertices[4][2] + y
+			self.mesh:setVertices(vertices)
+		end,
+		draw = function (self) love.graphics.draw(self.mesh) end
+	})
+end
+
+local Shapelist = {}
+
+function Shapelist.new(in_table, map, layer, offset) -- offset == {x = 'number', y = 'number', z = 'number'}
+	local k = 1
+	local m = 0
+	local shapes = {}
+
+	local x, y = centerize(offset.x, offset.y - offset.z)
+
+	for i = 1, layer.height do
+		for j = 1, layer.width do
+
+			table.insert(shapes, {
+				shape = map.HC:addPolygon(
+					x - map.Data.tilewidth / 2, y,
+					x, y + map.Data.tileheight / 2,
+					x + map.Data.tilewidth / 2, y,
+					x, y - map.Data.tileheight / 2
+				),
+				move = function (self, u, v)
+					local vertices = self.shape._polygon.vertices
+					print('1', inspect(vertices[1]))
+					print('2', inspect(vertices[2]))
+					print('3', inspect(vertices[3]))
+					print('4', inspect(vertices[4]))
+					vertices[1].x = vertices[1].x + u
+					vertices[1].y = vertices[1].y + v
+					vertices[2].x = vertices[2].x + u
+					vertices[2].y = vertices[2].y + v
+					vertices[3].x = vertices[3].x + u
+					vertices[3].y = vertices[3].y + v
+					vertices[4].x = vertices[4].x + u
+					vertices[4].y = vertices[4].y + v
+				end
+			})
+
+			if k % layer.width == 0 then
+				m = m + 1
+			end
+			k = k + 1
+			x = x + map.Data.tilewidth / 2
+			y = y + map.Data.tileheight / 2
+		end
+		x, y = centerize(offset.x, offset.y - offset.z)
+		y = y + (map.Data.tileheight / 2) * m
+		x = x - (map.Data.tilewidth / 2) * m
+	end
+	table.insert(in_table, shapes)
+end
+
 -- OBJECTS MANIPULATION
 
 function editor:addMap()
@@ -12,6 +105,7 @@ function editor:addMap()
 	self.map.Data.tilewidth = self.tilewidth
 	self.map.Data.tileheight = self.tileheight
 	self.map.Shapes = {}
+	self.map.Meshes = {}
 
 	self.step = self.tileheight / 2
 	self.mouse = self.map.HC:addCircle(0, 0, 1)
@@ -46,8 +140,18 @@ function editor:addLayer()
 	local layer = self.map.Data.layers[index]
 	self:createLayer(layer)
 
-	table.insert(self.map.Shapes, buildfullshapes_fix(layer, self.map, self.x, self.y, self.z))
+	Shapelist.new(self.map.Shapes, self.map, layer, {x = self.x, y = self.y, z = self.z})
 	self.z = self.z + self.step
+
+	local img = love.graphics.newImage('Images/bricks.jpg')
+	img:setFilter('nearest', 'nearest')
+
+	for k,v in pairs(self.map.Shapes) do
+		table.insert(self.map.Meshes, {})
+		for key, val in pairs(v) do
+			Meshlist.new(self.map.Meshes[#self.map.Meshes], img, val.shape._polygon)
+		end
+	end
 end
 
 function editor:move(x, y)
@@ -62,38 +166,57 @@ end
 
 function editor:reset_key(key, dt)
 	if key == 'up' then
-		self.map.Data.tileheight = self.map.Data.tileheight - 1 * dt * 10
-	elseif key == 'pageup' then
-		self.map.Data.tileheight = self.map.Data.tileheight - 1 * dt * 10
-		self.step = self.map.Data.tileheight / 2
-	elseif key == 'down' then
-		self.map.Data.tileheight = self.map.Data.tileheight + 1 * dt * 10
-	elseif key == 'pagedown' then
-		self.map.Data.tileheight = self.map.Data.tileheight + 1 * dt * 10
-		self.step = self.map.Data.tileheight / 2
+		-- self.map.Data.tileheight = self.map.Data.tileheight - 1 * dt * 10
+		return 0, -100
+	-- elseif key == 'pageup' then
+	-- 	self.map.Data.tileheight = self.map.Data.tileheight - 1 * dt * 10
+	-- 	self.step = self.map.Data.tileheight / 2
+	-- elseif key == 'down' then
+	-- 	self.map.Data.tileheight = self.map.Data.tileheight + 1 * dt * 10
+	-- elseif key == 'pagedown' then
+	-- 	self.map.Data.tileheight = self.map.Data.tileheight + 1 * dt * 10
+	-- 	self.step = self.map.Data.tileheight / 2
 	elseif key == 'wu' then
-		self.map.Data.tilewidth = self.map.Data.tilewidth + 2
-		self.map.Data.tileheight = self.map.Data.tileheight + 1
+		return 2, 1
 	elseif key == 'wd' then
-		self.map.Data.tilewidth = self.map.Data.tilewidth - 2
-		self.map.Data.tileheight = self.map.Data.tileheight - 1
+		return -2, -1
 	end
 end
 
 function editor:reset(key, dt)
-	self.map.HC:clear()
+	-- self.map.HC:clear()
 	self.z = 0
-	self.map.Shapes = {}
+	-- self.map.Shapes = {}
 
-	self:reset_key(key, dt)
-	for k,layer in pairs(self.map.Data.layers) do
-		table.insert(self.map.Shapes, buildfullshapes_fix(layer, self.map, self.x, self.y, self.z))
-		self.z = self.z + self.step
+	local u,v = self:reset_key(key, dt)
+	print(u, v)
+
+	for k, layer in pairs(self.map.Shapes) do
+		for key, val in pairs(layer) do
+			val:move(u, v)
+		end
 	end
+
+
+	-- for k,layer in pairs(self.map.Data.layers) do
+	-- 	Shapelist.new(self.map.Shapes, self.map, layer, {x = self.x, y = self.y, z = self.z})
+	-- 	self.z = self.z + self.step
+	-- end
+
+	-- local img = love.graphics.newImage('Images/bricks.jpg')
+	-- img:setFilter('nearest', 'nearest')
+	-- self.map.Meshes = {}
+
+	-- for k,v in pairs(self.map.Shapes) do
+	-- 	table.insert(self.map.Meshes, {})
+	-- 	for key, val in pairs(v) do
+	-- 		Meshlist.new(self.map.Meshes[#self.map.Meshes], img, val.shape._polygon)
+	-- 	end
+	-- end
 end
 
 function editor:update_input(dt)
-	if love.mouse.isDown('r') then print(inspect(self, {depth = 1})) end
+	if love.mouse.isDown('r') then print(inspect(self, {depth = 4})) end
 
 	if love.mouse.isDown('l') then
 		local x_diff = love.mouse.getX() - self.x_start
@@ -149,9 +272,9 @@ end
 
 function editor:collect_draw()
 	if #self.collected > 0 then
-		for key, shape in pairs(self.collected[#self.collected]) do
-			if shape:collidesWith(self.mouse) then
-				self.toDraw = shape
+		for key, val in pairs(self.collected[#self.collected]) do
+			if val.shape:collidesWith(self.mouse) then
+				self.toDraw = val
 				return
 			end
 		end
@@ -163,7 +286,6 @@ end
 function editor:init(Collider)
 	self.State = 'editor'
 	self.Collider = Collider
-	self.mode = 'collect'
 	self.x = 0
 	self.y = 0
 	self.z = 0
@@ -177,8 +299,17 @@ function editor:update(dt)
 	self:update_input(dt)
 	self.mouse:moveTo(love.mouse.getPosition())
 
-	if self.x_move or self.y_move then
+	if self.x_move ~= 0 or self.y_move ~= 0 then
 		self:move(self.x_move, self.y_move)
+
+		-- MOVE MESHES
+		if self.map.Meshes then
+		for k,v in pairs(self.map.Meshes) do
+			for key, val in pairs(v) do
+				val:move(self.x_move * 10, self.y_move * 10)
+			end
+		end
+		end
 	end
 
 	self:collect()
@@ -186,28 +317,17 @@ function editor:update(dt)
 end
 
 function editor:draw()
-	if self.mode == 'collect' then
-		if self.collected then
-			for key, val in pairs(self.collected) do
-				for k, v in pairs(val) do
-					v:draw()
-				end
-			end
-		end
-	end
-
-	if self.mode == 'global' then
-		if self.map then
-			for key, val in pairs(self.map.Shapes) do
-				for k, v in pairs(val) do
-					v:draw()
-				end
+	if self.collected then
+		for key, val in pairs(self.collected) do
+			for k, v in pairs(val) do
+				v.shape:draw()
+				-- self.map.Meshes[key][k]:draw()
 			end
 		end
 	end
 
 	if self.toDraw then
-		self.toDraw:draw('fill')
+		self.toDraw.shape:draw('fill')
 	end
 end
 
